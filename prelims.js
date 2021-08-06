32e357130569b18816024f034570d6aab4e3dd6e
bgdn-r <bogdanristic95@gmail.com>
2021-08-07 00:13:46 +0200

    prelims

--- prelims.js
+++ prelims.js
@@ -1,17 +1,21 @@
 class Prelims {
+  #numOfPrelimPlayers;
   constructor(numOfPlayers, playersArr, validNum) {
-    this.numOfPlayers = numOfPlayers;
+    this.N = numOfPlayers;
     this.playersArr = playersArr;
     // NOTE Sort from bigger to lower
     this.validNumsArr = validNum.sort((a, b) => b - a);
   }
 
   getPlayersForPrelims() {
-    const closestLowerNum = this.validNumsArr.find((num) => num < N);
-    const numOfPrelimPlayers = N - (closestLowerNum - 1);
+    const closestLowerNum = this.validNumsArr.find((num) => num < this.N);
+    this.#numOfPrelimPlayers = this.N - (closestLowerNum - 1);
+    console.log(closestLowerNum);
+    console.log(this.#numOfPrelimPlayers);
   }
 
   logger() {
+    this.getPlayersForPrelims();
     console.log(this.validNumsArr);
   }
 }
